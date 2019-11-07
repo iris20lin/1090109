bcbff4ce2c96597a818e9e46fc7a0df889f4f815
Li <gjo4us06c961115@gmail.com>
2019-11-07 19:46:47 +0800

    Tower

---
+++ ConsoleApp1/ConsoleApp1/Game.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Game
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public int Sub(int c, int d)
+        {
+            return c - d;
+        }
+        public int Fs(int g, int h)
+        {
+            int result = 0;
+            if (h==0)
+            {
+                Console.WriteLine("ㄎㄎ");
+            }
+            else
+            {
+              result= g / h;
+            }
+            return result;
+        }
+            public int Yed(int q, int w)
+        {
+            return q * w;
+        }
+
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+            int s1 = game.Add(3, 5);
+            int s2 = game.Sub(7, 2);
+            int s3 = game.Fs(3, 0);
+            int s4 = game.Yed(5, 4);
+            Console.WriteLine("3+5=" + s1);
+            Console.WriteLine("7-2=" + s2);
+            Console.WriteLine("3/0=" + s3);
+            Console.WriteLine("5*4=" + s4);
+            Console.ReadKey();
+        }
+    }
+}
